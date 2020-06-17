cddcc85d5b83fc71d425ecf1cb95cd88ec6a127e
verza22 <zuritabal@gmail.com>
2020-06-16 22:36:22 -0500

    Flutter peliculas

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,42 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+
+  String _apikey    = 'b63a04ecec7da50a9603f7bbadf2711b';
+  String _url       = 'api.themoviedb.org';
+  String _language  = 'es-ES';
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+
+    final url = Uri.https(_url, "3/movie/now_playing", {
+      'api_key' : _apikey,
+      'language': _language
+    });
+
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    
+    final url = Uri.https(_url, "3/movie/popular", {
+      'api_key' : _apikey,
+      'language': _language
+    });
+
+    return await _procesarRespuesta(url);
+  }
+
+}
